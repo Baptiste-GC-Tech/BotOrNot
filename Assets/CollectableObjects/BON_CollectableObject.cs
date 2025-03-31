59c3aa7b7c210cdd1f7594bbfa97e34562e53b0c
wdamour <wdamour@gaming.tech>
2025-03-31 13:46:51 +0200

    [ADD] Collectable Objects

---
+++ Assets/CollectableObjects/BON_CollectableObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BON_CollectableObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+/*        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Take();
+        }*/
+    }
+    public void Take()
+    {
+
+    }
+
+    public void Give()
+    {
+
+    }
+}
