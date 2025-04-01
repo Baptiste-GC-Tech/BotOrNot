bbcb2f39f03dda8fdbbf0e6a4bb91e7e7814fe8b
BCLEY <bcleymand@gaming.tech>
2025-04-01 14:43:38 +0200

    plateformes à liste fonctionnelles
    ouhouhou

--- Assets/Level Elements/MovingObjects/BON_MovObj_ListBased.cs
+++ Assets/Level Elements/MovingObjects/BON_MovObj_ListBased.cs
@@ -17,33 +17,31 @@ public class BON_MovObj_ListBased : BON_Actionnable
     protected bool _isCyclingPositive = false;
 
     private int _currentTargetIndex = 0;
+    protected int _previousTargetIndex;
+    protected float _currentTotalDistance;
+    protected Vector3 _currentDirection;
+    protected Vector3 _currentRotation;
+
+
     protected int CurrentTargetIndex {  
         get { return _currentTargetIndex; }
         set {
+
+            _previousTargetIndex = _currentTargetIndex;
+
             if (value >= _transformsList.Count) { 
                 if (_looping) { _currentTargetIndex = 0; } 
-                else { _currentTargetIndex = _transformsList.Count - 1; }  }
+                else { _status = false; _isCyclingPositive = false; }  }
             else if (value < 0) { 
                 if (_looping) { _currentTargetIndex = _transformsList.Count - 1; } 
-                else { _currentTargetIndex = 0; } }
-            else { _currentTargetIndex = value; } 
-        } 
-    }
+                else { _status = false; _isCyclingPositive = true; } }
+            else { _currentTargetIndex = value; }
 
-    protected int PreviousTargetIndex { 
-        get { 
-            if (_isCyclingPositive) { 
-                if (_currentTargetIndex == 0) { return _transformsList.Count - 1; } 
-                else { return _currentTargetIndex - 1; } 
-            } 
-            else {
-                if (_currentTargetIndex == _transformsList.Count - 1) { return 0; }
-                else { return _currentTargetIndex + 1; }
-            }
+            _currentDirection = (_transformsList[_currentTargetIndex].position - _transformsList[_previousTargetIndex].position).normalized;
+            _currentRotation = _transformsList[_currentTargetIndex].rotation.eulerAngles - _transformsList[_previousTargetIndex].rotation.eulerAngles;
+            _currentTotalDistance = (_transformsList[_currentTargetIndex].position - _transformsList[_previousTargetIndex].position).magnitude;
         }
-    } 
-    
-
+    }
 
     override public void On()
     {
@@ -78,14 +76,23 @@ public class BON_MovObj_ListBased : BON_Actionnable
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (_status) 
         {
             float oneSpeed = _moveSpeed * Time.deltaTime;
 
-            
-
+            if (oneSpeed >= (_transformsList[_currentTargetIndex].position - gameObject.transform.position).magnitude)
+            {
+                gameObject.transform.SetPositionAndRotation(_transformsList[_currentTargetIndex].position, _transformsList[_currentTargetIndex].rotation);
+                if (_isCyclingPositive) { CurrentTargetIndex++; } else { CurrentTargetIndex--; }
+            }
+            else
+            {
+                float movementFraction = oneSpeed / _currentTotalDistance;
+                gameObject.transform.position += _currentDirection * oneSpeed;
+                gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles + _currentRotation * movementFraction);
+            }
         }
     }
 }
