650752a75df6892c36d73485146fcbb51b133401
PortgasDRopy <117282513+PortgasDRopy@users.noreply.github.com>
2025-04-02 09:58:12 +0200

    rename timer + hook rework

---
+++ Assets/Scenes/BON_MovObj_Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BON_MovObj_Timer : BON_Actionnable
+{
+    //FIELD//
+    [SerializeField] float _maxTimer;
+    private float _currentTimer;
+    [SerializeField] float _extensionMax;
+
+
+
+    //CLASS METHODS
+    override public void Off()
+    {
+        _currentTimer = 0;
+    }
+
+
+
+    //UNITY METHODS
+    private void Start()
+    {
+        _currentTimer = _maxTimer;
+    }
+    private void FixedUpdate()
+    {
+        if (Status || _currentTimer < _maxTimer)
+        {
+            if (gameObject.transform.localScale.x < _extensionMax)
+            {
+                gameObject.transform.localScale += new Vector3(Time.deltaTime, 0, 0);
+            }
+        }
+        else
+        {
+            if (gameObject.transform.localScale.x > 0)
+            {
+                gameObject.transform.localScale -= new Vector3(Time.deltaTime, 0, 0);
+            }
+        }
+        if (_currentTimer < _maxTimer)
+        {
+            _currentTimer += Time.deltaTime;
+        }
+    }
+}
