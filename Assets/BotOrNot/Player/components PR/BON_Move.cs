dab44b4af152dd4f561a34e13fb14c3dbe738802
Baptiste-GC-Tech <117664376+Baptiste-GC-Tech@users.noreply.github.com>
2025-04-04 18:11:36 +0200

    bug gone, magik

--- Assets/BotOrNot/Player/components PR/BON_Move.cs
+++ Assets/BotOrNot/Player/components PR/BON_Move.cs
@@ -7,6 +7,7 @@ using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.ProBuilder;
+using UnityEngine.Rendering;
 
 // TODO: Implement the pause of accelaration and speed update when in the air
 public class BON_Move : MonoBehaviour
@@ -16,21 +17,21 @@ public class BON_Move : MonoBehaviour
      */
     public LayerMask deez;
 
-    private InputAction MoveAction;
-    Vector2 moveInputValue;
+    private InputAction _MoveAction;
+    Vector2 _moveInputValue;
 
-    /* Curve related */
-    private float _timeSinceAccelStart, _timeSinceDeccelStart;
-    private float _maxAccelTime, _maxDeccelTime;
+    ///* Curve related */
+    //private float _timeSinceAccelStart, _timeSinceDeccelStart;
+    //private float _maxAccelTime, _maxDeccelTime;
 
     /* Speed related */
     [SerializeField] float _maxSpeed;
-    [SerializeField] AnimationCurve _slopedSpeedCurve;
+    [SerializeField] AnimationCurve _SpeedMultiplierOverSlope;   // The axis represent the y component of the normal's value
     private float _curSpeed;
 
     /* Accelartion related */
-    [SerializeField] AnimationCurve _AccelCurve;
-    [SerializeField] AnimationCurve _DeccelCurve;
+    [SerializeField] AnimationCurve _AccelOverSpeed;
+    [SerializeField] AnimationCurve _DeccelOverSpeed;
 
     /* Direction related */
     private int _moveXAxisDir;
@@ -43,13 +44,13 @@ public class BON_Move : MonoBehaviour
      */
     void Start()
     {
-        MoveAction = InputSystem.actions.FindAction("ActionsMapPR/Move");
+        _MoveAction = InputSystem.actions.FindAction("ActionsMapPR/Move");
 
-        /* Curve fields setup */
-        _maxAccelTime = _AccelCurve.keys[_AccelCurve.keys.Length - 1].time;
-        _maxDeccelTime = _DeccelCurve.keys[_DeccelCurve.keys.Length - 1].time;
-        _timeSinceAccelStart = 0.0f;
-        _timeSinceDeccelStart = _maxDeccelTime;
+        ///* Curve fields setup */
+        //_maxAccelTime = _AcceleratingSpeedCurve.keys[_AcceleratingSpeedCurve.keys.Length - 1].time;
+        //_maxDeccelTime = _DecceleratingSpeedCurve.keys[_DecceleratingSpeedCurve.keys.Length - 1].time;
+        //_timeSinceAccelStart = 0.0f;
+        //_timeSinceDeccelStart = _maxDeccelTime;
     }
 
     void Update()
@@ -58,9 +59,9 @@ public class BON_Move : MonoBehaviour
         UpdateGroundNormal();
 
         /* Handles the input */
-        moveInputValue = MoveAction.ReadValue<Vector2>();
-        UpdateMoveDirFromInput(moveInputValue);
-        UpdateCurSpeed(moveInputValue);
+        _moveInputValue = _MoveAction.ReadValue<Vector2>();
+        UpdateMoveDirFromInput();
+        UpdateCurSpeed();
 
 
         /* Applies the movement */
@@ -71,48 +72,44 @@ public class BON_Move : MonoBehaviour
      *  CLASS METHODS
      */
     // Calculates the current speed
-    private void UpdateCurSpeed(Vector2 ARGinputValue)
+    private void UpdateCurSpeed()
     {
-        switch (Mathf.Approximately(moveInputValue.x, 0.0f))
+        
+        float deFactoMaxSpeed = _maxSpeed * Mathf.Abs(_moveInputValue.x) * _SpeedMultiplierOverSlope.Evaluate(_groundNormalVect.y);  // This speed depends on the intensity of the player's input
+        float speedDelta = deFactoMaxSpeed - _curSpeed;
+
+        Debug.Log("defactoMax - cur = delta : " + deFactoMaxSpeed + " - " + _curSpeed + " = " + speedDelta);
+
+        // Case in which we are below our de facto max speed : we want to go faster
+        if (speedDelta > 0.0f)
         {
-            // Case in which we accelerate
-            case false:
-                /* Updates the timers (both in case the input starts and stops very quickly) */
-                _timeSinceAccelStart = Mathf.Clamp(_timeSinceAccelStart + Time.deltaTime, 0.0f, _maxAccelTime);
-                _timeSinceDeccelStart = Mathf.Clamp(_timeSinceDeccelStart - Time.deltaTime, 0.0f, _maxDeccelTime);
-
-                // Use the acceleration timer to set the speed
-                _curSpeed = _maxSpeed * _AccelCurve.Evaluate(_timeSinceAccelStart);
-                break;
-
-            // Case in which we deccelerate
-            case true:
-                /* Updates the timers (both in case the input starts and stops very quickly) */
-                _timeSinceAccelStart = Mathf.Clamp(_timeSinceAccelStart - Time.deltaTime, 0.0f, _maxAccelTime);
-                _timeSinceDeccelStart = Mathf.Clamp(_timeSinceDeccelStart + Time.deltaTime, 0.0f, _maxDeccelTime);
-
-                // Use the decceleration timer to set the speed
-                _curSpeed = _maxSpeed * _DeccelCurve.Evaluate(_timeSinceDeccelStart);
-                break;
+            _curSpeed += _AccelOverSpeed.Evaluate(_curSpeed) * _maxSpeed * Time.deltaTime;
+            _curSpeed = Mathf.Clamp(_curSpeed, 0.0f, deFactoMaxSpeed);
         }
+        // Case in which we are above our de facto max speed : we want to go slower
+        if (speedDelta < 0.0f)
+        {
+            _curSpeed -= _DeccelOverSpeed.Evaluate(_curSpeed) * _maxSpeed * Time.deltaTime;
+            _curSpeed = Mathf.Clamp(_curSpeed, 0.0f, _maxSpeed);
+        }
+
+        //// Applies the slope multiplier
+        //_curSpeed *= _SpeedMultiplierOverSlope.Evaluate(_groundNormalVect.y);
+        Debug.Log(_SpeedMultiplierOverSlope.Evaluate(_groundNormalVect.y));
     }
 
     // Calculates the current movement direction induced by a player input 
-    private void UpdateMoveDirFromInput(Vector2 ARGinputValue)
+    private void UpdateMoveDirFromInput()
     {
         // Case in which there is no input : don't touch anything
-        if (Mathf.Approximately(ARGinputValue.x, 0.0f) && Mathf.Approximately(ARGinputValue.y, 0.0f)) return;
+        if (Mathf.Approximately(_moveInputValue.x, 0.0f) && Mathf.Approximately(_moveInputValue.y, 0.0f)) return;
 
-        _moveXAxisDir = moveInputValue.x > 0.0f ? 1 : -1;
+        _moveXAxisDir = _moveInputValue.x > 0.0f ? 1 : -1;
 
         // Case of a flat ground : uses either forward directions instead of doing math
         if (Mathf.Approximately(_groundNormalVect.y, 1.0f)) _curMoveDir = Vector3.right * _moveXAxisDir;
         // Case of a sloped ground : finds the tangent to the normal depending on which direction we are going towards, and applies a speed debuff
-        else
-        {
-            _curMoveDir = _moveXAxisDir == 1 ? Vector3.Cross(_groundNormalVect, Vector3.forward) : Vector3.Cross(Vector3.forward, _groundNormalVect);
-            _curMoveDir *= _slopedSpeedCurve.Evaluate(_groundNormalVect.y);
-        }
+        else _curMoveDir = _moveXAxisDir == 1 ? Vector3.Cross(_groundNormalVect, Vector3.forward) : Vector3.Cross(Vector3.forward, _groundNormalVect);
 
         //Debug.Log("moveDirThisFrame : " + moveDirThisFrame);
     }
