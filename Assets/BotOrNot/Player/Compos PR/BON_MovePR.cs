01e9de594f3f02041edc838731105a0e306df5cd
NykoPa <npautal@gaming.tech>
2025-04-18 11:09:31 +0200

    bwa

--- Assets/BotOrNot/Player/Compos PR/BON_MovePR.cs
+++ Assets/BotOrNot/Player/Compos PR/BON_MovePR.cs
@@ -231,7 +231,7 @@ public class BON_MovePR : MonoBehaviour
         }
 
         // Bounce
-        if (!_player.AvatarState.IsGrounded && (_fallHeight.y - transform.position.y) >= _heightBonceStart && !_isBouncing)
+        if ((_fallHeight.y - transform.position.y) >= _heightBonceStart && !_isBouncing)
         {
             Debug.Log("Should enter bounce");
             _isBouncing = true;
