0342fc38a840830826e235e803f15fcc94853a11
EBER0039 <enzobertrand@gaming.tech>
2025-04-22 12:08:28 +0200

    Tests

---
+++ Assets/BotOrNot/LevelEvent/Events/BON_LE_EyeCatcher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BON_LE_EyeCatcher : BON_LevelEvent
+{
+    public override void Happen() 
+    {
+
+    }
+}
