27fdf2157610c47de054052faa1df47e6a395d75
BCLEY <bcleymand@gaming.tech>
2025-04-23 14:54:31 +0200

    s

--- Assets/BotOrNot/Player/State Machine/States/BON_SDrift.cs
+++ Assets/BotOrNot/Player/State Machine/States/BON_SDrift.cs
@@ -5,14 +5,11 @@ public class BON_SDrift : BON_State
     public override void Enter()
     {
         //Debug.LogWarning("Enterring the Drift");
-        _player.GetComponent<BON_MovePR>().ShouldNotMove = true;
-        _player.GetComponent<BON_MovePR>().CurSpeed = 0;
         _player.AvatarState.IsDrifting = false;
     }
 
     public override void Exit()
     {
-        _player.GetComponent<BON_MovePR>().ShouldNotMove = false;
         _player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         _player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
     }
